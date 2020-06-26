9d007305b6ea99868fde7321a9343918fc0d66b2
Lima <victordevtb@gmail.com>
2020-06-26 11:24:27 -0300

    teste

--- index.js
+++ index.js
@@ -72,7 +72,13 @@ app.post("/not",(req, res) => {
         MercadoPago.payment.search({
             qs: filtro
         }).then(data => {
-            console.log(data);
+            var pagamento = data.body.results[0];
+
+            if(pagamento != undefined){
+                console.log(pagamento);
+            }else{
+                console.log("Pagamento não existe!");
+            }
         }).catch(err => {
             console.log(err);
         });
